f5986096bc1106b5a4e9a535d254606b75812fa5
Islam Udeen <you@example.com>
2018-12-16 17:56:06 +0000

    Register page started

---
+++ create-account.php
@@ -0,0 +1,8 @@
+<?php
+//$pdo = new PDO('mysql:host=127.0.01:dbname')
+?>
+
+<h1>Register</h1>
+<form action="create-account.php" method="post">
+<input type="text" name="username" value="" placeholder="Username...">
+</form>
