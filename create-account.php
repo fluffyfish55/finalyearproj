211f9d030e7b2e584d971d89332040bb69dfac91
fluffyfish55 <35689766+fluffyfish55@users.noreply.github.com>
2019-04-05 11:02:26 +0100

    Delete create-account.php

--- create-account.php
+++
@@ -1,8 +0,0 @@
-<?php
-//$pdo = new PDO('mysql:host=127.0.01:dbname')
-?>
-
-<h1>Register</h1>
-<form action="create-account.php" method="post">
-<input type="text" name="username" value="" placeholder="Username...">
-</form>
